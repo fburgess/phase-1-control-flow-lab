eec0c41d53cace274bd52dd46f5551b950c327a6
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-02-08 14:30:54 -0500

    commit tree 58666263055862be1c3bb14a4c6c406de228e003

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//If statment 
+function scuberGreetingForFeet(number) {
+  if (number <= 400){
+    return 'This one is on me!';
+  }
+  if (number >= 2001 && number < 2500) {
+    return 'I will gladly take your thirty bucks.' ;
+  }
+  if (number >= 2501) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//Ternary Method
+function ternaryCheckCity(city) {
+  const result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//Switch Method 
+function switchOnCharmFromTip(tip){
+  let goodTip, regTip;
+  switch (tip) {
+    case tip = 'generous':
+      return goodTip = "Thank you so much."
+    case tip = 'not as generous':
+      return regTip = "Thank you."
+    default: 
+    return 'Bye.'
+  }
 }
+
